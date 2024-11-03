4bb5e111f2bea567fb003b55713c531c2f346a3b
shamimranapro85 <shamimranaprofessional@gmail.com>
2024-11-03 20:08:07 +0600

    upload repo first time

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className='text-red-500 text-2xl text-center'> Welcome our gadget heaven site </h1>
+    </>
+  )
+}
+
+export default App
